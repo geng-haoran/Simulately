6f791d9acd160f31be8924694d0054d37fc8809e
Aiden <i@aidenli.net>
2023-11-11 11:13:59 +0800

    fixed: related work tag bugs

--- src/pages/related/index.tsx
+++ src/pages/related/index.tsx
@@ -28,7 +28,7 @@ const TITLE = translate({message: 'Related Work'});
 const DESCRIPTION = translate({
   message: 'List of work powered by simulators.',
 });
-const SUBMIT_URL = 'https://github.com/facebook/docusaurus/discussions/7826';
+const SUBMIT_URL = 'https://github.com/geng-haoran/Simulately/discussions/2';
 
 type UserState = {
   scrollTopPosition: number;
@@ -242,35 +242,6 @@ function ShowcaseCards() {
     <section className="margin-top--lg margin-bottom--xl">
       {filteredUsers.length === sortedPapers.length ? (
         <>
-          {/* <div className={styles.showcaseFavorite}>
-            <div className="container">
-              <div
-                className={clsx(
-                  'margin-bottom--md',
-                  styles.showcaseFavoriteHeader,
-                )}>
-                <Heading as="h2" id='isaacgym-papers'>
-                  <FavoriteIcon svgClass={styles.svgIconFavorite} />
-                  <Translate id="showcase.isaacgymsList.title">
-                    Isaac-Gym Papers
-                  </Translate>
-                  <span className={clsx(styles.countPapers,)}>
-                  </span>
-                </Heading>
-                <SearchBar />
-              </div>
-              <ul
-                className={clsx(
-                  'container',
-                  'clean-list',
-                  styles.showcaseList,
-                )}>
-                {isaacgymUsers.map((user) => (
-                  <ShowcaseCard key={user.title} user={user} />
-                ))}
-              </ul>
-            </div>
-          </div> */}
           <div className="container margin-top--lg">
             <Heading as="h2" className={styles.showcaseHeader} id='all-papers'>
               <Translate id="showcase.usersList.allUsers">All papers</Translate>
@@ -308,7 +279,7 @@ export default function Showcase(): JSX.Element {
   return (
     <Layout title={TITLE} description={DESCRIPTION}>
       <main className="margin-vert--lg">
-        {/* <AddMyWorkButton /> */}
+        <AddMyWorkButton />
         <ShowcaseFilters />
         <ShowcaseCards />
       </main>
